1ef60037364f678f2810df3fc4aab0d304b271bb
tahmina-nishu <akthertahmina572@gmail.com>
2024-11-21 13:21:40 +0600

    basic setup and logo

---
+++ src/routes/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import HomeLayout from '../Layout/HomeLayout';
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout></HomeLayout>
+    },
+    {
+        path: "/news",
+        element: <h1>News Layout</h1>
+    },
+    {
+        path: "/auth",
+        element: <h1>Login</h1>
+    },
+    {
+        path: "*",
+        element: <h1>Error</h1>
+    },
+])
+
+export default Router;
